832db8e190db1a3863863f11dc1fce820f234736
Siddhesh <siddhespotdar4787@gmail.com>
2024-11-10 00:25:00 +0530

    feat: add order edit & delete functionality

--- Backend/src/routes/order.routes.js
+++ Backend/src/routes/order.routes.js
@@ -1,9 +1,15 @@
 import Router from "express";
 import { verifyToken } from "../middleware/auth.middleware.js";
-import { createOrder } from "../controllers/order.controller.js";
+import {
+  createOrder,
+  deleteOrder,
+  updateOrder,
+} from "../controllers/order.controller.js";
 
 const router = Router();
 
 router.route("/create").post(verifyToken, createOrder);
+router.route("/update").put(verifyToken, updateOrder);
+router.route("/delete").delete(verifyToken, deleteOrder);
 
 export default router;
